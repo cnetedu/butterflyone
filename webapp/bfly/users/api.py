e97d1c9188dedcc5e30cafbb3dcd402bc2ede20b
George Sequeira <george.m.sequeira@gmail.com>
2019-08-14 17:27:06 -0400

    Adding users api POST and test

--- webapp/bfly/users/api.py
+++ webapp/bfly/users/api.py
@@ -1,38 +1,57 @@
 from flask import Blueprint, request, jsonify, render_template, flash, redirect, url_for
 from flask_restplus import Api, Resource, fields
+from werkzeug import secure_filename
+
 import bfly.users.forms
 import bfly.users.models
+import flask
 import random
 
 
 users = Blueprint('users', __name__, template_folder="templates")
 api = Api(users)
-ns_conf = api.namespace('users', description="Users")
+ns_conf = api.namespace('api', description="Users")
+
 
 @users.route('/dashboard', methods=["GET"])
 def dashboard():
-    users, cursor = bfly.users.models.list_users(10, None)
+    users, cursor = bfly.users.models.list_users(100, None)
     return render_template('users.html', title='Users', users=users)
 
 
-@users.route('/creation', methods=['GET', 'POST'])
+@users.route('/upload', methods=['GET', 'POST'])
 def creation():
     form = bfly.users.forms.UserCreation()
+
     if form.validate_on_submit():
         user = bfly.users.models.User(
-            id="{}".format(random.randint(1,10000)),
+            id=form.email.data,
             fullname=form.fullname.data,
             email=form.email.data,
-            major1=form.major1.data,
-            major2=form.major2.data,
-            major3=form.major3.data,
+            phone=form.phone.data,
+            major=form.major.data,
             college=form.college.data,
             graduationMonth=form.graduationMonth.data,
             graduationYear=form.graduationYear.data,
+            gpa=form.gpa.data,
+            currentLocation=form.currentLocation.data,
+            desiredLocation1=form.desiredLocation1.data,
+            desiredLocation2=form.desiredLocation2.data,
+            desiredLocation3=form.desiredLocation3.data,
         )
 
         bfly.users.models.db.session.add(user)
         bfly.users.models.db.session.commit()
+
+        resume = bfly.users.models.Resume(
+            data=form.binaryResume.data.stream.read(),
+            name=form.binaryResume.data.name,
+            content_type=form.binaryResume.data.content_type,
+            id=user.id
+        )
+        bfly.users.models.db.session.add(resume)
+        bfly.users.models.db.session.commit()
+
         flash("You successfully created a user.")
         return redirect(url_for("users.dashboard"))
 
@@ -42,15 +61,23 @@ def creation():
 user_model = api.model('User', {
     'id': fields.String,
     'fullname': fields.String,
+    'phone': fields.String,
     'email': fields.String,
-    'major1': fields.String,
-    'major2': fields.String,
-    'major3': fields.String,
+    'major': fields.String,
     'college': fields.String,
     'graduationMonth': fields.Integer,
-    'graduationYear': fields.Integer
+    'graduationYear': fields.Integer,
+    'gpa': fields.String,
+    'currentLocation': fields.String,
+    'desiredLocation1': fields.String,
+    'desiredLocation2': fields.String,
+    'desiredLocation3': fields.String
 })
 
+list_of_users = api.model("ListOfUsers", {
+    'users': fields.List(fields.Nested(user_model)),
+    'token': fields.String
+})
 
 user_update = api.model('UserUpdate', {
     'fullname': fields.String(),
@@ -62,6 +89,11 @@ user_update = api.model('UserUpdate', {
     'graduationYear': fields.Integer
 })
 
+user_creation = api.model('UserCreation', {
+    'fullname': fields.String,
+    'phone': fields.String,
+    'email': fields.String
+})
 
 @ns_conf.route('/<string:id>')
 @ns_conf.response(404, 'User not found.')
@@ -93,13 +125,35 @@ class User(Resource):
             ns_conf.abort(404)
 
 
+@ns_conf.route('/resume/<string:id>')
+class Resume(Resource):
+    def get(self, id):
+        resume_data = bfly.users.models.get_resume(id)
+        if resume_data:
+            response = flask.make_response(resume_data.data, 200)
+            response.headers['content-type'] = resume_data.content_type
+            return response
+        else:
+            ns_conf.abort(404)
+
+    def put(self, id):
+        pass
+
+
+def validate_required_creation(data_dict):
+    for key in ['fullname', 'phone', 'email']:
+        if key not in data_dict:
+            raise ValueError("{} required in data".format(key))
+
+
 @ns_conf.route('')
 class UserList(Resource):
-    @ns_conf.marshal_list_with(user_model)
+    @ns_conf.marshal_list_with(list_of_users)
     def get(self):
-        return bfly.users.models.list_users(), 200
+        users, next_page = bfly.users.models.list_users()
+        return dict(users=users, token=next_page), 200
 
-    @ns_conf.expect(user_model, validate=True)
+    @ns_conf.expect(user_creation, validate=True)
     @ns_conf.response(201, "User created successfully")
     def post(self):
         """
@@ -110,16 +164,12 @@ class UserList(Resource):
         if 'id' in data:
             del data['id']
 
+        validate_required_creation(data)
         user = bfly.users.models.User(
-            id="{}".format(random.randint(1,10000)),
+            id=data['email'],
             fullname=data['fullname'],
-            email=data['email'],
-            major1=data.get('major1', ''),
-            major2=data.get('major2', ''),
-            major3=data.get('major3', ''),
-            college=data.get('college', ''),
-            graduationMonth=data.get('graduationMonth', '5'),
-            graduationYear=data.get('graduationYear', '2021'),
+            phone=data['phone'],
+            email=data['email']
         )
 
         bfly.users.models.db.session.add(user)
--- webapp/bfly/users/forms.py
+++ webapp/bfly/users/forms.py
@@ -1,21 +1,96 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField, FileRequired
 from wtforms import PasswordField, StringField, SubmitField, ValidationError, IntegerField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 import bfly.users.models
+import os
+import phonenumbers
+
+script_dir = os.path.dirname(__file__)
+majors_rel_path = 'list_of_majors.txt'
+majors = []
+with open(os.path.join(script_dir, majors_rel_path)) as o:
+    for line in o:
+        majors.append(line.strip())
+
+cities_rel_path = "list_of_cities.txt"
+cities = []
+with open(os.path.join(script_dir, cities_rel_path)) as o:
+    for line in o:
+        cities.append(line.strip())
+
+universities_rel_path = "list_of_universities.txt"
+universities = []
+with open(os.path.join(script_dir, universities_rel_path)) as o:
+    for line in o:
+        universities.append(line.strip())
+
+
+def location_check(form, field):
+    if field.data not in cities:
+        raise ValidationError('{} is not a valid city.'.format(field.data))
 
 
 class UserCreation(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     fullname = StringField('FullName', validators=[DataRequired()])
-    major1 = StringField('First Major', validators=[DataRequired()])
-    major2 = StringField('Second Major', validators=[])
-    major3 = StringField('Third Major', validators=[])
+    phone = StringField('Phone', validators=[])
+    major = StringField('Major', validators=[DataRequired()])
     college = StringField('College', validators=[DataRequired()])
-    graduationMonth = IntegerField('Graduation Month')
-    graduationYear = IntegerField('Graduation Year')
+
+    graduationMonth = IntegerField('Graduation Month', validators=[DataRequired()])
+    graduationYear = IntegerField('Graduation Year', validators=[DataRequired()])
+    gpa = StringField('GPA', validators=[DataRequired()])
+    currentLocation = StringField('CurrentLocation', validators=[DataRequired(), location_check])
+    desiredLocation1 = StringField('DesiredLocation1', validators=[DataRequired(), location_check])
+    desiredLocation2 = StringField('DesiredLocation2', validators=[DataRequired(), location_check])
+    desiredLocation3 = StringField('DesiredLocation3', validators=[DataRequired(), location_check])
+    binaryResume = FileField('Resume', validators=[FileRequired(),
+                                                   FileAllowed(['pdf', 'docx', 'doc', 'txt'],
+                                                               'Only pdf, docx, doc, txt, supported!')])
     submit = SubmitField('Store')
 
+
     def validate_email(self, field):
-        if bfly.users.models.User.query.filter_by(email=field.data).first():
+        if bfly.users.models.User.query.filter_by(id=field.data).first():
             raise ValidationError("Email already in use")
+
+    def validate_phone(self, field):
+        if len(field.data) > 16:
+            raise ValidationError('Invalid phone number.')
+        try:
+            input_number = phonenumbers.parse(field.data)
+            if not (phonenumbers.is_valid_number(input_number)):
+                raise ValidationError('Invalid phone number.')
+        except:
+            input_number = phonenumbers.parse("+1"+field.data)
+            if not (phonenumbers.is_valid_number(input_number)):
+                raise ValidationError('Invalid phone number.')
+
+    def validate_major(self, field):
+        if field.data not in majors:
+            raise ValidationError("{} is not a valid major.".format(field.data))
+
+    def validate_college(self, field):
+        if field.data not in universities:
+            raise ValidationError("{} is not a valid college/university".format(field.data))
+
+    def validate_graduationMonth(self, field):
+        if field.data < 0 or field.data > 12:
+            raise ValidationError("{} is not a valid month.".format(field.data))
+
+    def validate_graduationYear(self, field):
+        if field.data < 2019:
+            raise ValidationError("{} is in the past".format(field.data))
+
+        if field.data > 2030:
+            raise ValidationError("{} is too far in the future".format(field.data))
+
+    def validate_gpa(self, field):
+        if len(field.data) > 3 or '.' not in field.data:
+            raise ValidationError("{} is not in the expected format X.Y".format(field.data))
+
+        first_digit, second_digit = field.data.split('.')
+        if int(second_digit) < 0 or int(first_digit) < 0 or int(first_digit) > 4:
+            raise ValidationError("GPA is out of 4.0")
